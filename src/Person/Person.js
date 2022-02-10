2df8ab0413b53580182467744c000beaf908177d
EmilyRacheal <emilychealy4@gmail.com>
2022-02-10 15:06:41 +0100

    first commit

---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+// const person = (props) =>{
+//     return <p>Hello I'm {props.name} and I am {props.age} years old!</p>
+// }
+
+const person = (props) =>{
+    return (
+        <div>
+            <p>Hello I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
+}
+
+export default person;   
