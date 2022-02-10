1efff780e4e256511a1a98f80c7a9d1fa9cf87a2
EmilyRacheal <emilychealy4@gmail.com>
2022-02-10 15:50:53 +0100

    using use state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import person from './Person/Person';
 import Person from './Person/Person';
 
-class App extends Component{
-  state = {
-    persons: [
-      {name: 'max', age: 28 },
-      {name: 'milly', age: 23 },
-      {name: 'vee', age: 15 }
-    ],
-    otherState: 'some other value'
-  }
 
-switchNameHandler = () => {
-  // console.log('was collected')
-  // donot do this megthodthis.state.persons[0].name = 'EmilyRacheal'
-  this.setState({
-    persons: [
-    {name: 'maxff', age: 28 },
-    {name: 'milly', age: 23 },
-    {name: 'veerr', age: 20 }
-    ]
-  })
-}
-
-  render() {
+  const App = props =>{
+    const [personState, setPersonState] = useState({ 
+      persons: [
+        {name: 'max', age: 28 },
+        {name: 'milly', age: 23 },
+        {name: 'vee', age: 15 }
+      ],
+      otherState: 'some other value'
+    })
     return (
           <div className="App">
             <h2>welcome</h2>
@@ -34,17 +21,21 @@ switchNameHandler = () => {
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
             <Person name ={this.state.persons[1].name} age={this.state.persons[1].age} />
             <Person name= {this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Coding, VolleyBalling</Person>
-            {/* <Person name= "Milly" age="19"> 
-              <ul>
-                <li>racing</li>
-                <li>cooking</li>
-              </ul>
-            
-             </Person> */}
           </div>
         );
     // return React.createElement('div',null, React.createElement('h1', null, 'does this work now?'));
   }
-}
 
 export default App;
+
+switchNameHandler = () => {
+// console.log('was collected')
+// donot do this megthodthis.state.persons[0].name = 'EmilyRacheal'
+this.setState({
+  persons: [
+  {name: 'maxff', age: 28 },
+  {name: 'milly', age: 23 },
+  {name: 'veerr', age: 20 }
+  ]
+})
+}
